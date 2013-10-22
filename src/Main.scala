234435746befb5f6a0b185ca954176926698517e
Miike Ramos <miike.ramos@ticketfly.com>
2013-10-21 18:00:36 -0700

    Setting up a demo for learning Scala!

---
+++ src/Main.scala
@@ -0,0 +1,20 @@
+object Main extends App {
+
+  println("Let's learn Scala!")
+
+  println("\n")
+
+  println("Part 1.2 - Variables")
+
+  val greeting = "Hello, world"
+  println(greeting)
+
+  var note = "Note that variables declared as val are constants and are immutable"
+  println(note)
+
+  note = "Variables declared as var are mutable"
+  println(note)
+
+
+
+}
